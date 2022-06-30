e270ea7316f62d266663bf7ca8f980dcffdab029
gTorque8999 <gabriel.leguizamon22@gmail.com>
2022-06-30 16:18:10 -0300

    cards corregidas

--- js/scripts.js
+++ js/scripts.js
@@ -26,20 +26,23 @@ const generadorDeCards = (productoARecorrer) => {
 let acumuladorCardsProductos = ``;
 productos.forEach((producto) => {
     acumuladorCardsProductos += `
-    <div class="card product-item border-0 mb-4">
-        <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-            <img class="img-fluid w-100" src="" alt="notebook DELL">
-        </div>
-        <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-            <h6 class="text-truncate mb-3">${producto.titulo}</h6>
-            <div class="d-flex justify-content-center">
-                <h6>${producto.precio}</h6><h6 class="text-muted ml-2"><del>$200000</del></h6>
-            </div>
-        </div>
-        <div class="card-footer d-flex justify-content-between bg-light border">
-            <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Ver detalle</a>
-            <a onclick ="agregarProductoAlCarrito(${producto.id})" class="btn btn-sm text-dark p-0" ><i  class="fas fa-shopping-cart text-primary mr-1"></i>Agregar al carrito</a>
-    </div>`
+    <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div class="card product-item border-0 mb-4">
+                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                        <img class="img-fluid w-100" src="img/product-8.jpg" alt="">
+                    </div>
+                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                        <h6 class="text-truncate mb-3">${producto.titulo}</h6>
+                        <div class="d-flex justify-content-center">
+                            <h6>${producto.precio}</h6><h6 class="text-muted ml-2"><del>$123.00</del></h6>
+                        </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between bg-light border">
+                        <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>View Detail</a>
+                        <a onclick ="agregarProductoAlCarrito(${producto.id})" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
+                    </div>
+                </div>
+            </div>`
 })
 
 document.getElementById('card-container').innerHTML = acumuladorCardsProductos;
