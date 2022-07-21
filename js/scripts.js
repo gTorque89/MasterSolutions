c350ae5aaf09281bb23e4013847ffd2adc1ec8ad
gTorque8999 <gabriel.leguizamon22@gmail.com>
2022-07-20 22:16:13 -0300

    intentando fetchear un productos.json

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,23 @@ const carritoAcumulado = JSON.parse(localStorage.getItem('carrito')) || [];
 const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
 
 //Listado de productos
-let productos = [
-    {titulo: 'Laptop DELL', id: 01, precio: 180000, categoria: 'Laptops', stock: 20, img: "./img/productsStart/notebookDell.webp", descuento: 8},
-    {titulo: 'Laptop HP', id: 02, precio: 190000, categoria: 'Laptops', stock: 15, img: './img/productsStart/notebookHp.jpg', descuento: 5},
-    {titulo: 'Laptop Acer', id: 03, precio: 225000, categoria: 'Laptops', stock: 10, img: './img/productsStart/notebookAcer.webp', descuento: 6},
-    {titulo: 'Laptop Asus', id: 04, precio: 280000, categoria: 'Laptops', stock: 12, img: './img/productsStart/notebookAsus.webp', descuento: 2},
-    {titulo: 'Laptop Lenovo', id: 05, precio: 150000, categoria: 'Laptops', stock: 18, img: './img/productsStart/notebookLenovo.jpg', descuento: 4},
-    {titulo: 'Laptop Exo', id: 06, precio: 175000, categoria: 'Laptops', stock: 15, img: './img/productsStart/notebookExo.webp', descuento: 8},
-    {titulo: 'Laptop Banghó', id: 07, precio: 240000, categoria: 'Laptops', stock: 25, img: './img/productsStart/notebookBangho.jpg', descuento: 9},
-    {titulo: 'Laptop Alienware', id: 08, precio: 320000, categoria: 'Laptops', stock: 10, img: './img/productsStart/notebookAlienware.jpg', descuento: 12},
-    {titulo: 'Tablet Atom', id: 09, precio: 20000, categoria: 'Tablets', stock: 8, img: './img/productsStart/tabletAtom.jpg', descuento: 2},
-    {titulo: 'Tablet DELL', id: 10, precio: 15000, categoria: 'Tablets', stock: 10, img: './img/productsStart/tabletDell.jpg', descuento: 10},
-    {titulo: 'Tablet Lenovo', id: 11, precio: 18000, categoria: 'Tablets', stock: 10, img: './img/productsStart/tabletLenovo.jpg', descuento: 7},
-    {titulo: 'Tablet Samsung', id: 12, precio: 15000, categoria: 'Tablets', stock: 6, img: './img/productsStart/tabletSamsung.jpg', descuento: 4},
-    {titulo: 'Tablet Exo', id : 13, precio: 18500, categoria: 'Tablets', stock: 18, img: './img/productsStart/tabletExo.jpg', descuento: 15}, 
-    {titulo: 'Tablet Samsung Tab', id: 14, precio: 17000, categoria: 'Tablets', stock: 14, img: './img/productsStart/tablet-galaxy.png', descuento: 16},
-    {titulo: 'Tablet Asus', id: 15, precio: 25000, categoria: 'Tablets', stock: 7, img: './img/productsStart/tabletAsus.jpeg', descuento: 2},
-    {titulo: 'Tablet Banghó', id: 16, precio: 13000, categoria: 'Tablets', stock: 25, img: './img/productsStart/tabletBangho.jpg', descuento: 8},
-    {titulo: 'Pc Gamer Ryzen 5', id: 17, precio: 255000, categoria: 'Desktop', stock: 7, img: './img/productsStart/notebookHp.jpg', descuento: 4},
-    {titulo: 'Pc Oficina Ryzen 3', id: 18, precio: 130200, categoria: 'Desktop', stock: 25, img: './img/productsStart/notebookHp.jpg', descuento: 7}
+async function obtenerProductos() {
+    const productos = await fetch('productos.json')
+    .then((response) => response.json())
+    .then((data) => data);
+    return productos;
 
-];
-//Generador de cards index
-const generadorDeCards = (productoARecorrer) => {
-    let acumuladorCardsProductos = ``;
+}
+
+//Generador de cards
+
+function mostrarProductos(productos) {
+    const listado = document.getElementById("card-container-ofertas");
+    listado.innerHTML = "";
+    
     productos.forEach((producto) => {
-        acumuladorCardsProductos += `
+        const { titulo, precio, img, id } = producto;
+        const productoHTML = `
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
                     <div class="card product-item border-0 mb-4">
                         <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
@@ -47,17 +38,23 @@ const generadorDeCards = (productoARecorrer) => {
                             <a onclick ="agregarProductoAlCarrito(${producto.id})" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Añadir al carrito</a>
                         </div>
                     </div>
-                </div>
-                `
-    })
-    
-    document.getElementById('card-container-ofertas').innerHTML = acumuladorCardsProductos;
+                </div>`;
+    listado.innerHTML += productoHTML;
+    });
 }
-    generadorDeCards();
+
+function escuchadorBotonMostrarProductos() {
+    document.getElementById("mostrar-productos").addEventListener("click", async () => {
+    const productos = await obtenerProductos();
+    mostrarProductos(productos);
+    });
+    
+}
+escuchadorBotonMostrarProductos();
 
 
 
-//Filtrar categoria
+/*//Filtrar categoria
     
 const obtenerProductosPorCategoria = (categoria) => {
     return new Promise((resolve, reject) => {
@@ -72,7 +69,7 @@ obtenerProductosPorCategoria('Laptops').then((productos) => {
     const nuevoListadoFiltrado = productos;
     generadorDeCards(nuevoListadoFiltrado)
 })
-
+*/
 //Sección agregar a favoritos
 function popUpAgregasteAFavoritos () {
     Toastify({
@@ -91,7 +88,7 @@ function popUpAgregasteAFavoritos () {
     }).showToast();
 }
 
-const agregarProductoAFavoritos = (id) => {
+function agregarProductoAFavorito (id) {
     
         const favoritoSeleccionado = productos.find((producto) => producto.id === id);
         favoritos.push(favoritoSeleccionado)
@@ -102,7 +99,49 @@ const agregarProductoAFavoritos = (id) => {
     document.getElementById('total-favoritos').innerHTML = favoritos.length + " - Fav";
     
     popUpAgregasteAFavoritos()
+    cardsFavoritos(favoritos)
+
 };
+
+
+function cardsFavoritos() {
+    let acumuladorCardsFavoritos = ``;
+    favoritos.forEach((producto) => {
+    acumuladorCardsFavoritos += `              
+                        <tr>
+                            <td class="align-middle"><img src="${producto.img}" alt="${producto.titulo}" style="width: 50px;">${producto.titulo}</td>
+                            <td class="align-middle">$${producto.precio}</td>
+                            <td class="align-middle"><button  onclick="eliminarItemDeCarrito(${producto.id}) "id="eliminar-item" class="btn btn-sm btn-primary" id="eliminar-item" ><i class="fa fa-times"></i></button></td>
+                        </tr>
+                        `
+    //DOM cards de modal                  
+    document.getElementById('tarjeta-detalle-favoritos').innerHTML = acumuladorCardsFavoritos;
+    const btnEliminarItemDeCarrito = document.getElementById('eliminar-item');
+    
+    btnEliminarItemDeCarrito.onclick = () => {
+        eliminarItemDeFavorito()
+    };   
+    })
+}
+cardsFavoritos()
+
+
+function eliminarItemDeFavorito (id) {
+
+    const productoSeleccionado = listaFavoritos.findIndex((producto) => producto.id === id);
+    favoritos.splice(productoSeleccionado, 1);
+    const totalCarrito = carritoAcumulado.reduce((acumulador, producto) => acumulador - producto.precio, 0);
+    document.getElementById('total-favoritos').innerHTML = favoritos.length + " - $" + totalCarrito;
+
+    const btnEliminarItemDeCarrito = document.getElementById('eliminar-item');
+    btnEliminarItemDeCarrito.onclick = () => {
+        eliminarItemDeFavorito(id)
+    };
+    localStorage.setItem('favoritos', JSON.stringify(favoritos))
+    cardsCarrito()
+    actualizarDom()
+    cardsCarrito()
+}
 
 //Sección agregar al carrito
 
@@ -183,7 +222,8 @@ function cardsCarrito() {
 }
 cardsCarrito(carritoAcumulado)
 
-//Abrir modal
+//Abrir modal de carrito /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     const btnAbrirModal = document.getElementById('abrir-modal');
     const btnCerrarModal = document.getElementById('cerrar-modal');
     const modal = document.getElementById('modal-carrito');
@@ -200,12 +240,35 @@ cardsCarrito(carritoAcumulado)
         modal.close();
     })
 
+//Abrir modal de favoritos ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const btnAbrirFavoritos = document.getElementById('abrir-favoritos');
+const btnCerrarFavoritos = document.getElementById('cerrar-modal-favoritos');
+const modalFavoritos = document.getElementById('modal-favoritos');
+
+btnAbrirFavoritos.addEventListener('click', () => {
+    modalFavoritos.showModal();
+})
+
+btnCerrarFavoritos.addEventListener('click', () => {
+    modalFavoritos.close()
+})
 
 
+function generarCardsFavoritos () {
+    cardsCarrito()
+
+    document.getElementById('tarjeta-detalle').innerHTML = acumuladorCardsModal;
+    const btnEliminarItemDeCarrito = document.getElementById('eliminar-item');
+    
+    btnEliminarItemDeCarrito.onclick = () => {
+        eliminarItemDeCarrito()
+    };   
+
+}
 
 
-
-function ordenarPrecio() {
+/*function ordenarPrecio() {
 const ordenarPorPrecio = productos.sort((a, b) => {
     if (a.precio > b.precio) {
         return 1;
@@ -221,3 +284,4 @@ const btnOrdenarPrecio = document.getElementById('ordenar-precio');
             ordenarPrecio()
         };    
 const listadoOrdenado =[];
+*/
